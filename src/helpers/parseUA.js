c6857271090cbfe4fac9daaa67d4cb5910e028da
Ayush shrivastav <ayush.shriv.dev@gmail.com>
2026-09-04 20:08:41 +0000

    refactor(parseUA): streamline user agent parsing and fix device type assignment

--- src/helpers/parseUA.js
+++ src/helpers/parseUA.js
@@ -1,35 +1,31 @@
 import { UAParser } from "ua-parser-js";
 
-export const parseUA = userAgent => {
-    if (!userAgent.includes("KHTML")) {
-        userAgent = `Mozilla/5.0 (Linux; Android 13; SM-M326B) AppleWebKit/537.36 
-(KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36`;
-    }
-    const parser = new UAParser(userAgent);
-    let deviceType = null;
-    const result = parser.getResult();
-    if (userAgent.includes("Mobile")) {
-        deviceType = "mobile";
-    } else if (
-        userAgent.includes("tablet") ||
-        userAgent.includes("ipad") ||
-        userAgent.includes("sm-t") ||
-        userAgent.includes("xoom") ||
-        userAgent.includes("silk") ||
-        userAgent.includes("kindle")
-    ) {
-        deviceType = "tab";
-    } else {
-        deviceType = "deskopt";
-    }
-    return {
-        browser: result.browser.name,
-        browserVersion: result.browser.version,
-        os: result.os.name,
-        deviceType,
-        userAgent,
-        deviceName: `${result.browser.name} on ${result.os.name}`,
-        osVersion: result.os.version,
-        deviceModel: result.device.model
-    };
+export const parseUA = (userAgent) => {
+  const parser = new UAParser(userAgent);
+  let deviceType = null;
+  const result = parser.getResult();
+  if (userAgent.includes("Mobile")) {
+    deviceType = "mobile";
+  } else if (
+    userAgent.includes("tablet") ||
+    userAgent.includes("ipad") ||
+    userAgent.includes("sm-t") ||
+    userAgent.includes("xoom") ||
+    userAgent.includes("silk") ||
+    userAgent.includes("kindle")
+  ) {
+    deviceType = "tab";
+  } else {
+    deviceType = "desktop";
+  }
+  return {
+    browser: result.browser.name,
+    browserVersion: result.browser.version,
+    os: result.os.name,
+    deviceType,
+    userAgent,
+    deviceName: `${result.browser.name} on ${result.os.name}`,
+    osVersion: result.os.version,
+    deviceModel: result.device.model,
+  };
 };
